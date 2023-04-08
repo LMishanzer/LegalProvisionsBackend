66a21088362770e336d06744d1afc991e338fa39
Mike Leskiv <leskivmisha@gmail.com>
2023-04-08 02:19:40 +0200

    Date helper

---
+++ LegalProvisionsLib/Helpers/DateHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace LegalProvisionsLib.Helpers;
+
+public static class DateHelper
+{
+    public static DateOnly DateTimeToDate(DateTime dateTime)
+    {
+        return new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
+    }
+}
